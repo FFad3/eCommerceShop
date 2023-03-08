cff6fa0394ac186f076b0c7b976f59c4384c10b6
Patryk Gneba <gnebapatryk@gmail.com>
2023-03-08 19:05:56 +0100

    add products card component

---
+++ src/app/product-card/product-card.component.ts
@@ -0,0 +1,11 @@
+import { Component,Input } from '@angular/core';
+import { product } from '../models/product';
+
+@Component({
+  selector: 'product-card',
+  templateUrl: './product-card.component.html',
+  styleUrls: ['./product-card.component.css']
+})
+export class ProductCardComponent {
+  @Input() item : product;
+}
